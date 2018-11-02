e421e6b4cfd65cd75e825682935d1260b2a657cc
nesarares <nesa.rares@gmail.com>
2018-11-02 09:36:45 +0200

    Changed login background, removed register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './core/components/login/login.component';
-import { RegisterComponent } from './core/components/register/register.component';
 import { HomeComponent } from './core/components/home/home.component';
 
 const routes: Routes = [
@@ -12,10 +11,6 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
   }
 ];
 
@@ -23,4 +18,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { SharedModule } from '../shared/shared.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
-  declarations: [
-    HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    NavbarComponent
-  ],
-  exports: [HomeComponent, LoginComponent, RegisterComponent, NavbarComponent]
+  declarations: [HomeComponent, LoginComponent, NavbarComponent],
+  exports: [HomeComponent, LoginComponent, NavbarComponent]
 })
 export class CoreModule {}
