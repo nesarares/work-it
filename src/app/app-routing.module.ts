87bbc89d64aac6146b43ae2e187171cd0503e094
georgemihali97 <georgemihali97@yahoo.com>
2018-11-11 16:29:41 +0200

    Created Register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './core/components/login/login.component';
 import { HomeComponent } from './core/components/home/home.component';
+import { RegisterComponent } from './core/components/register/register.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from '../shared/shared.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
-  declarations: [HomeComponent, LoginComponent, NavbarComponent],
+  declarations: [HomeComponent, LoginComponent, NavbarComponent, RegisterComponent],
   exports: [HomeComponent, LoginComponent, NavbarComponent]
 })
 export class CoreModule {}
