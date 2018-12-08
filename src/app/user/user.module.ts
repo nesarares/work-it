b43a21278106d5397b68212c86c021133444a695
edionu97 <onu_edy@yahoo.com>
2018-12-08 21:42:57 +0200

    Removed unecessary import

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,11 +5,10 @@ import { UserRoutingModule } from './user-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { UserHomeComponent } from './components/user-home/user-home.component';
 import { JobDetailsComponent } from './components/job-details/job-details.component';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 
 @NgModule({
-  imports: [CommonModule, SharedModule, UserRoutingModule, InfiniteScrollModule],
+  imports: [CommonModule, SharedModule, UserRoutingModule],
   declarations: [
     DashboardComponent,
     UserHomeComponent,
