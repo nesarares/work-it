12456c30758d64e75f8059930284e3a6d02cf181
nesarares <nesa.rares@gmail.com>
2019-01-17 13:41:30 +0200

    Added time ago to notifications

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { JsonpModule } from '@angular/http';
+import { ParticlesModule } from 'angular-particle';
+import { PasswordStrengthMeterModule } from 'angular-password-strength-meter';
+import { apiKeys } from 'src/environments/apiKeys';
+import { TimeAgoPipe } from 'time-ago-pipe';
+
+import { SharedModule } from '../shared/shared.module';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { SharedModule } from '../shared/shared.module';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { RegisterComponent } from './components/register/register.component';
-import { PasswordStrengthMeterModule } from 'angular-password-strength-meter';
-import { FooterComponent } from './components/footer/footer.component';
-import { ParticlesModule } from 'angular-particle';
 import { NavbarMobileComponent } from './components/navbar-mobile/navbar-mobile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
-import { JsonpModule } from '@angular/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ContactComponent } from './components/contact/contact.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AgmCoreModule } from '@agm/core';
-import { apiKeys } from 'src/environments/apiKeys';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   imports: [
@@ -37,7 +38,8 @@ import { apiKeys } from 'src/environments/apiKeys';
     FooterComponent,
     NavbarMobileComponent,
     PrivacyPolicyComponent,
-    ContactComponent
+    ContactComponent,
+    TimeAgoPipe
   ],
   exports: [
     HomeComponent,
