166c1ba77e497e2db18d55619d7f2ef66184a838
nesarares <nesa.rares@gmail.com>
2019-01-18 12:47:13 +0200

    Changed default photo

--- src/app/shared/constants/urls.ts
+++ src/app/shared/constants/urls.ts
@@ -1,3 +1,4 @@
 export const urls = {
-	defaultPhoto: 'https://content.recklessnetwork.com/monthly_2016_08/dogavatar3.jpg.0bea41affc676bce724cb2c9f5ed9f15.thumb.jpg.ff828b4ca69782411f405ab1c11493ba.jpg'
-}
+  defaultPhoto:
+    'http://www.zimphysio.org.zw/wp-content/uploads/2018/01/default-avatar-2.jpg'
+};
